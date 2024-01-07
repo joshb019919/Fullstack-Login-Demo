b91b469d1f6637515238b1391f229662900b025f
Josh Borthick <46645462+joshb019919@users.noreply.github.com>
2024-01-06 23:11:15 -0600

    Initial commit and first draft of Login Demo

---
+++ main/java/edu/missouristate/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package edu.missouristate.repository;
+
+import edu.missouristate.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ main/java/edu/missouristate/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package edu.missouristate.service;
+
+import edu.missouristate.domain.CustomUserDetails;
+import edu.missouristate.domain.User;
+import edu.missouristate.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optionalUsers = usersRepository.findByEmail(email);
+        optionalUsers.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers.map(CustomUserDetails::new).get();
+    }
+
+    public User getLogginInUser() {
+        String email;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            email = ((CustomUserDetails) principal).getEmail();
+        } else {
+            email = principal.toString();
+        }
+        if (email != null && !email.isEmpty()) {
+            Optional<User> opt = usersRepository.findByEmail(email);
+            return opt.orElse(null);
+        }
+        return null;
+    }
+}
